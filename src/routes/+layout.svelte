5c3d376cb691401f92d78b602067c5518a539cec
Jamin <jaminwebdev@gmail.com>
2025-04-18 13:53:41 -0500

    initial scaffolding of spa mode

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<header>
+	<a href="#/">Home</a>
+	<a href="#/example">Example</a>
+	<a href="#/login">Login</a>
+</header>
+{@render children()}
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<h1>Login route</h1>
