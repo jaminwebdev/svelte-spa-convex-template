f634d61b325ee98f08cd29dcdedeae8278a83c8a
Jamin <jaminwebdev@gmail.com>
2025-04-18 21:31:03 -0500

    header and redirect components moved

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { SignedIn, UserButton } from 'svelte-clerk/client';
+	import ThemeToggle from '@/lib/components/ThemeToggle.svelte';
+</script>
+
+<header class="flex justify-end p-5">
+	<SignedIn>
+		<div class="flex items-center gap-6">
+			<ThemeToggle />
+			<div class="flex gap-3">
+				<UserButton />
+			</div>
+		</div>
+	</SignedIn>
+</header>
