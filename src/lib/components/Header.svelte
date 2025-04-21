1210c60687e56436652a6755e1ee5ec7446d0c93
Jamin <jaminwebdev@gmail.com>
2025-04-21 09:42:32 -0500

    Header refactor

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
 	import { SignedIn, UserButton } from 'svelte-clerk/client';
 	import ThemeToggle from '@/lib/components/ThemeToggle.svelte';
+	import { Boxes } from '@lucide/svelte';
+	import Container from './Container.svelte';
 </script>
 
-<header class="flex justify-end p-5">
-	<SignedIn>
-		<div class="flex items-center gap-6">
-			<ThemeToggle />
-			<div class="flex gap-3">
-				<UserButton />
+<div class="border-b-primary/50 border-1">
+	<Container classes="max-w-[88rem]">
+		<header class=" flex justify-between p-5">
+			<div class="flex items-center gap-1">
+				<Boxes size="32" />
+				<span class="text-2xl font-black">Site Logo</span>
 			</div>
-		</div>
-	</SignedIn>
-</header>
+			<SignedIn>
+				<div class="flex items-center gap-6">
+					<ThemeToggle />
+					<div class="flex gap-3">
+						<UserButton />
+					</div>
+				</div>
+			</SignedIn>
+		</header>
+	</Container>
+</div>
