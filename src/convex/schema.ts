15c7385723f7912c44e0efa99b4157287cafcdec
Jamin <jaminwebdev@gmail.com>
2025-04-21 13:50:58 -0500

    schema validation always on

--- src/convex/schema.ts
+++ src/convex/schema.ts
@@ -1,10 +1,15 @@
 import { defineSchema, defineTable } from 'convex/server';
 import { v } from 'convex/values';
 
-export default defineSchema({
-	tasks: defineTable({
-		text: v.string(),
-		isCompleted: v.boolean(),
-		date: v.optional(v.string())
-	})
-});
+export default defineSchema(
+	{
+		tasks: defineTable({
+			text: v.string(),
+			isCompleted: v.boolean(),
+			date: v.optional(v.string())
+		})
+	},
+	{
+		schemaValidation: true
+	}
+);
