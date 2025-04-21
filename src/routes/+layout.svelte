09a7cfd933419e14c6edb22c8ccabd26812d75bd
Jamin <jaminwebdev@gmail.com>
2025-04-21 13:26:11 -0500

    toaster added to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,13 @@
 	import { PUBLIC_CONVEX_URL, PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
 	import { ClerkProvider } from 'svelte-clerk/client';
 	import { setupConvex } from 'convex-svelte';
+	import { Toaster } from '$lib/components/ui/sonner/index.js';
 
 	const { children } = $props();
 	setupConvex(PUBLIC_CONVEX_URL);
 </script>
 
 <ClerkProvider publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}>
+	<Toaster />
 	{@render children()}
 </ClerkProvider>
