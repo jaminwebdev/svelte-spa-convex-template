82baf38d9ca062784c6d635de07f2805ec0e2b97
Jamin <jaminwebdev@gmail.com>
2025-04-21 09:33:59 -0500

    cleanup

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -3,7 +3,6 @@ import type { ServerInit } from '@sveltejs/kit';
 // This function runs once, when the server is created or the app starts in the browser
 // it is a useful place to do asynchronous work such as initializing a database connection.
 // this will delay hydration
-// init convex and clerk clients here
 export const init: ServerInit = async () => {
 	console.log('init hook running - do db inits here');
 };
