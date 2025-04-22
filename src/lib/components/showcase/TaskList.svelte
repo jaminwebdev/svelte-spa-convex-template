076e0168964a8fb2ddfe75c3969516db9f516559
Jamin <jaminwebdev@gmail.com>
2025-04-22 12:05:56 -0500

    skeleton loader

--- src/lib/components/showcase/TaskList.svelte
+++ src/lib/components/showcase/TaskList.svelte
@@ -2,6 +2,7 @@
 	import { useQuery } from 'convex-svelte';
 	import TaskItem from './TaskItem.svelte';
 	import { api } from '@/convex/_generated/api';
+	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
 
 	const { user_id } = $props<{ user_id: string }>();
 
@@ -9,7 +10,13 @@
 </script>
 
 {#if query.isLoading}
-	Loading...
+	<div class="flex items-center space-x-4">
+		<Skeleton class="h-12 w-12 rounded-full" />
+		<div class="space-y-2">
+			<Skeleton class="h-4 w-[250px]" />
+			<Skeleton class="h-4 w-[200px]" />
+		</div>
+	</div>
 {:else if query.error}
 	failed to load: {query.error.toString()}
 {:else}
