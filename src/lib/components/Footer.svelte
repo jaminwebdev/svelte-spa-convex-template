10937e40c83f97434d74d56aebff39cd940ce60d
Jamin <jaminwebdev@gmail.com>
2025-04-22 11:57:50 -0500

    Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,9 @@
+<footer class="border-b py-12">
+	<div class="mx-auto max-w-5xl px-6">
+		<div class="flex flex-wrap justify-center gap-6">
+			<span class="text-muted-foreground order-last block text-center text-sm md:order-first">
+				© {new Date().getFullYear()} Hebuildswebsites, All rights reserved
+			</span>
+		</div>
+	</div>
+</footer>
--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Header from '@/lib/components/Header.svelte';
+	import Footer from '@/lib/components/Footer.svelte';
 	import Redirect from '@/lib/components/Redirect.svelte';
 	import { SignedOut, SignedIn } from 'svelte-clerk/client';
 	const { children } = $props();
@@ -8,6 +9,7 @@
 <SignedIn>
 	<Header />
 	{@render children()}
+	<Footer />
 </SignedIn>
 <SignedOut>
 	<Redirect route="login" />
